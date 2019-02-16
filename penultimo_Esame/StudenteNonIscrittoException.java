a242c6bbe4ba963ac690dea93bc67286c557b0ff
cotin alexandru <a.cotin96@yahoo.com>
2019-02-16 09:14:50 +0100

    Delete StudenteNonIscrittoException.java

--- penultimo_Esame/StudenteNonIscrittoException.java
+++
@@ -1,7 +0,0 @@
-package it.univr.esami;
-
-public class StudenteNonIscrittoException extends Exception {
-	public StudenteNonIscrittoException(Studente studente) {
-		super("Lo studente " + studente.nome + " " + studente.cognome + " non è iscritto all'esame");
-	}
-}
