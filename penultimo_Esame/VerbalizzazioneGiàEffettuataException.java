13ea875e299f2209317bc3e6443f9d858329330d
cotin alexandru <a.cotin96@yahoo.com>
2019-02-16 09:14:57 +0100

    Delete VerbalizzazioneGiàEffettuataException.java

--- penultimo_Esame/VerbalizzazioneGiàEffettuataException.java
+++
@@ -1,7 +0,0 @@
-package it.univr.esami;
-
-public class VerbalizzazioneGiàEffettuataException extends Exception {
-	public VerbalizzazioneGiàEffettuataException(Studente studente) {
-		super("Lo studente " + studente.nome + " " + studente.cognome + " ha già verbalizzato");
-	}
-}
